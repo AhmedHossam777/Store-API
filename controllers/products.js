d5a6e2662353471ad93ea7a909a145772e0523b8
Ahmed Hossam <AhmedHossam9226@gmail.com>
2024-01-02 23:10:01 +0200

    add some features like sorting and searching

--- controllers/products.js
+++ controllers/products.js
@@ -1,25 +1,49 @@
+const e = require('express');
 const Product = require('../models/product');
 
-const getAllProducts = async (req, res) => {
-
-  const query = Product.find(req.query);
-  const products = await query.sort('name');
-  res.status(200).json({
-    message: 'success',
-    result: products.length,
-    products,
-  });
-};
-
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name');
-  
+  const products = await Product.find({}).sort('-name price');
+
   res.status(200).json({
     message: 'success',
     result: products.length,
     products,
   });
 };
+
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort } = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }; //? $options: 'i' -> case insensitive, $regex: name -> search for name even if it's not the full name
+  }
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    console.log(sortList);
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const product = await result;
+  res.status(200).json({
+    message: 'success',
+    result: product.length,
+    product,
+  });
+};
+
+
 
 // const getProduct = async (req, res) => {
 //   const { id: productID } = req.params;

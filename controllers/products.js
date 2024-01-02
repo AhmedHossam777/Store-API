ee7d2b9119290d8954574357d7079394f61c9fe9
Ahmed Hossam <AhmedHossam9226@gmail.com>
2024-01-03 00:00:24 +0200

    add select and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const e = require('express');
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).sort('-name price').limit(10).skip(5);
 
   res.status(200).json({
     message: 'success',
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
 
@@ -35,15 +35,23 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort('createdAt');
   }
-  const product = await result;
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    console.log(fieldsList);
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const product = await result.skip(skip).limit(limit);
   res.status(200).json({
     message: 'success',
     result: product.length,
     product,
   });
 };
-
-
 
 // const getProduct = async (req, res) => {
 //   const { id: productID } = req.params;
